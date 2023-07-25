9df6736a5ecac8ce04c90978a8860d7893636b63
abdullah <theabdullahchaudhary@gmail.com>
2023-07-25 19:36:36 +0500

    Update app.js

--- app.js
+++ app.js
@@ -26,15 +26,39 @@
 // new message code
 
 const express = require("express");
+// require("dotenv").config();
+// const app = express();
+
+// // route
+// app.get("/", (req, res) => {
+//   // Sending the updated message in the response
+//   res
+//     .status(200)
+//     .send("This is my first deployment on .js file, Work has been done.");
+// });
+
+// // Listening to the port 
+// let PORT = process.env.PORT ? process.env.PORT : 8000;
+// console.log(PORT);
+// app.listen(PORT, () => {
+//   console.log(`Server is running...`);
+// });
+
+
+
+
+// message update
+
+const express = require("express");
 require("dotenv").config();
 const app = express();
 
 // route
 app.get("/", (req, res) => {
-  // Sending the updated message in the response
+  // Sending This is the home page! in the page
   res
     .status(200)
-    .send("This is my first deployment on .js file, Work has been done.");
+    .send(`This is my first deployment on .js file, Work has been done ${process.env.PORT}`);
 });
 
 // Listening to the port 
