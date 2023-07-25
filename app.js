eea5a87d48925a4870c91ba6d6ea52e1dc3e5680
abdullah <theabdullahchaudhary@gmail.com>
2023-07-25 19:31:26 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 
+
+// const express = require("express");
+// require("dotenv").config();
+// const app = express();
+
+// // route
+// app.get("/", (req, res) => {
+//   // Sending This is the home page! in the page
+//   res
+//     .status(200)
+//     .send(`This is my first deployment on .js file. ${process.env.PORT} Work has been done. `);
+// });
+
+// // Listening to the port 
+// let PORT = process.env.PORT ? process.env.PORT : 8000;
+// console.log(PORT);
+// app.listen(PORT, () => {
+//   console.log(`Server is running...`);
+// });
+
+
+
+
+
+// new message code
+
 const express = require("express");
 require("dotenv").config();
 const app = express();
 
 // route
 app.get("/", (req, res) => {
-  // Sending This is the home page! in the page
+  // Sending the updated message in the response
   res
     .status(200)
-    .send(`This is my first deployment on .js file. ${process.env.PORT} Work has been done. `);
+    .send("This is my first deployment on .js file, Work has been done.");
 });
 
 // Listening to the port 
